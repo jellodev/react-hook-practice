acda92f64feb39425bf96e43efaaebe220983276
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 22:13:26 +0900

    UsePreventLeave hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
+};
+
 const App = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+
   return (
     <div className="App">
-      <button>Delete the world</button>
+      <button onClick={enablePrevent}>EnablePrevent</button>
+      <button onClick={disablePrevent}>DisablePrevent</button>
     </div>
   );
 };
