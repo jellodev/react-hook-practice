d0cfee8772273ac3c04a775817ee0ad7bdd922b9
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 21:52:11 +0900

    UseClick hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
+
+const useClick = (onclick) => {
+  const element = useRef();
+  useEffect(() => {
+    const currentElement = element.current;
+    if (typeof onclick !== "function") return;
+    if (currentElement) currentElement.addEventListener("click", onclick);
+
+    // componentWillUnMount
+    return () => {
+      if (currentElement) currentElement.removeEventListener("click", onclick);
+    };
+  }, []);
+  return element;
+};
 
 const App = () => {
-  const input = useRef();
-  setTimeout(() => input.current.focus(), 5000);
+  const sayHello = () => {
+    console.log("say Hello");
+  };
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <div>Hi</div>
-      <input ref={input} placeholder="test"></input>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 };
