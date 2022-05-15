fd13e4527b102fcfe9be0374393500d1996f2bff
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 23:51:20 +0900

    UseAxios hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useAxios } from "./UseAxios";
 
 const App = () => {
-  return <div className="App" style={{ height: "1000vh" }}></div>;
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://cors-anywhere.herokuapp.com/https://yts.mx/api/v2/list_movies.json",
+    data: {},
+    error: null,
+  });
+
+  console.log([data, error.message]);
+  console.log(loading ? "loading" : "done");
+
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
 };
 export default App;
---
+++ src/UseAxios.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const useAxios = (opts, axiosInstance = axios) => {
+  const [state, setState] = useState({
+    loading: true,
+    data: null,
+    error: null,
+  });
+
+  const [trigger, setTrigger] = useState(0);
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now());
+  };
+  useEffect(() => {
+    if (!opts.url) return;
+    axiosInstance(opts)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+  return state;
+};
