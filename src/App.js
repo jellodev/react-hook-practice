7bb570383942b6b0e95f1faf2ce2ea4010907fc0
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 20:43:25 +0900

    UserInput hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, {useState} from 'react';
+import React from "react";
+import { useInput } from "./UseInput";
 
 const App = () => {
-    const [item, setItem] = useState(1);
-    const incrementItem = () => setItem(item + 1);
-    const decrementItem = () => setItem(item - 1);
-    return (
-      <div>
-        <h1>hello {item}</h1>
-        <button onClick={incrementItem}>Increment</button>
-        <button onClick={decrementItem}>Decrement</button>
-      </div>
-    )
-}
+  const validator = (value) => !value.includes("@");
+  const name = useInput("jello", validator);
+  return (
+    <div>
+      <h1>hello</h1>
+      <input placeholder="Name" {...name}></input>
+    </div>
+  );
+};
 
 export default App;
