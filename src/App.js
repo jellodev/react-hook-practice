b0a94bc7e08f676693c0e95b623313f9f179c840
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 22:36:04 +0900

    UseFadeIn hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
-const useBeforeLeave = (onBefore) => {
-  //if (typeof onBefore !== "function") return;
-  const handle = (event) => {
-    const { clientY } = event;
-    if (clientY <= 0) onBefore();
-  };
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
   useEffect(() => {
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
+    if (typeof duration !== "number") return;
+    if (typeof delay !== "number") return;
+    if (!element.current) return;
+
+    const { current } = element;
+    current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+    current.style.opacity = 1;
   }, []);
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const begForLife = () => console.log("please dont leave");
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(5, 4);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>ha ha ha</p>
     </div>
   );
 };
