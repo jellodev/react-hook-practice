eaf3541f26fb50107cdbbb880ca5a3eb18badcc8
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 21:38:16 +0900

    useRef example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useEffect, useState } from "react";
-
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
-};
+import React, { useRef } from "react";
 
 const App = () => {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"), 5000);
+  const input = useRef();
+  setTimeout(() => input.current.focus(), 5000);
   return (
     <div className="App">
       <div>Hi</div>
+      <input ref={input} placeholder="test"></input>
     </div>
   );
 };
