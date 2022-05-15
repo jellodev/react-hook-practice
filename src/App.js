1d738b80edc4c063dda4afb98731a0cb2b77b3a5
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 20:18:18 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [item, setItem] = useState(1);
+    const incrementItem = () => setItem(item + 1);
+    const decrementItem = () => setItem(item - 1);
+    return (
+      <div>
+        <h1>hello {item}</h1>
+        <button onClick={incrementItem}>Increment</button>
+        <button onClick={decrementItem}>Decrement</button>
+      </div>
+    )
 }
 
 export default App;
