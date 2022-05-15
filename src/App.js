66916669147f9450cd0533bf9629f5b366dfb225
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 21:33:56 +0900

    UseTitle hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
 const App = () => {
-  const sayHello = () => console.log("hello");
-  const [number, setNumber] = useState(0);
-  const [secondNumber, setSecondNumber] = useState(0);
-  // ComponentDidMount, ComponentWillUnMount, ComponentDidUpdate
-  useEffect(sayHello, []);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
-      <div>hi</div>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setSecondNumber(secondNumber + 1)}>
-        {secondNumber}
-      </button>
+      <div>Hi</div>
     </div>
   );
 };
