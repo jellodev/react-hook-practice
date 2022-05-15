bed594f07747c4bb4428384b8e6f0bbcb87aca16
김미희 <jello@gimmihuiui-MacBook-Air.local>
2022-05-15 22:02:01 +0900

    UseConfirm hook 2 - add validation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
 
-const useConfirm = (message, callback, rejection) => {
-  if (typeof callback !== "function") return;
-  const confirmAction = () => {
-    if (window.confirm(message)) {
-      callback();
-    } else {
-      rejection();
-    }
-  };
-  return confirmAction;
-};
-
 const App = () => {
-  const deleteWorld = () => console.log("bye");
-  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
-  const abort = () => console.log("abort");
   return (
     <div className="App">
-      <button onClick={confirmDelete}>Delete the world</button>
+      <button>Delete the world</button>
     </div>
   );
 };
